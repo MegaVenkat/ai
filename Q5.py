baa314d1d11339502cec9c1aea3195c21291be41
Priyanshu Tiwari <you@example.com>
2022-01-18 12:08:14 +0530

    +1

--- Q5.py
+++ Q5.py
@@ -2,7 +2,7 @@ a=int(input("Enter first number : "))
 b=int(input("Enter second number : "))
 c=int(input("Enter third number : "))
 if a==b==c :
-    print("Sum is : "+3*a)
+    print("Sum is : "+str(3*(a+b+c)))
 else :
     print("Sum is : "+str(a+b+c))    
 
